9373e69cc633a03458f48809a2d49b9375e087b0
dengxin <994386508@qq.com>
2024-05-07 11:57:05 +0800

    fix: 修改

--- src/keyboardCell.tsx
+++ src/keyboardCell.tsx
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2024-05-07 11:55:50
+ * @LastEditors: dengxin 994386508@qq.com
+ * @LastEditTime: 2024-05-07 11:56:49
+ * @FilePath: /vehicle-plate-keyboard/src/keyboardCell.tsx
+ */
 import * as React from 'react';
 
 interface KeyboardCellProps {
@@ -17,12 +23,13 @@ const TypeToStyle: { [key in keyboardCellType]: string } = {
 };
 
 const KeyboardCell = React.memo((props: KeyboardCellProps) => {
-  const handleClick = () => {
+  const handleTouchEnd = (e:any) => {
+    e.preventDefault();
     if (!props.disabled && typeof props.onClick === 'function') {
       props.onClick(props.cell);
     }
   };
-
+  
   return (
     <section
       className={`${'keyboard-cell'} ${TypeToStyle[props.type]} ${
@@ -31,7 +38,7 @@ const KeyboardCell = React.memo((props: KeyboardCellProps) => {
       aria-disabled={props.disabled}
       aria-label={props.cell}
       role="button"
-      onClick={handleClick}
+      onTouchEnd={handleTouchEnd}
     >
       <span style={props.cellTextStyle} className="cell-text">
         {props.cell}
